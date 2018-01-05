66c57cb41a81990a4fa5cbc21cb53695c990b372
wangxiaofeng <xiaofeng1836@gmail.com>
2018-01-05 16:16:08 +0800

    0105

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
-// import * as BooksAPI from './BooksAPI'
+import * as BooksAPI from './BooksAPI'
+import ListBooks from './ListBooks'
 import './App.css'
 
 class BooksApp extends React.Component {
@@ -13,9 +14,22 @@ class BooksApp extends React.Component {
     showSearchPage: false
   }
 
+  state = {
+    books:[]
+  }
+
+  componentDidMount () {
+    BooksAPI.getAll().then((books) => {
+      this.setState({ books })
+    })
+  }
+
   render() {
     return (
       <div className="app">
+        <ListBooks
+          books={this.state.books}
+        />
         {this.state.showSearchPage ? (
           <div className="search-books">
             <div className="search-books-bar">
---
+++ src/ListBooks.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+class ListBooks extends Component {
+    render(){
+        console.log('Props', this.props)
+        return (
+        <div >
+        </div>
+        )
+    }
+}
+
+export default ListBooks
