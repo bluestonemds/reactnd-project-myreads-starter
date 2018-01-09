aaf066b62cde04c82fa7443173ac39309c7eeb14
王晓峰 <wangxiaofeng@wangxiaofengs-iMac.local>
2018-01-09 21:34:34 +0800

    0109

--- src/App.js
+++ src/App.js
@@ -71,76 +71,76 @@ class BooksApp extends React.Component {
     return (
      
       <div className="app">
-      <Link
-            to='/search'
-            className='pure-button search'
-            onClick={this.search}
-          >Search books</Link>
-      {this.state.showSearchPage && (
-        <Route exact path='/search' render={() => (
-          <div className="search-books">
-                <div className="search-books-bar">
-                <Link
-                  to='/'
-                  className='close-search'
-                  onClick={this.clearQuery}
-                ></Link>
-                <div className="search-books-input-wrapper">
-                  {/*
-                    NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                    You can find these search terms here:
-                    https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
+        <Link
+              to='/search'
+              className='pure-button search'
+              onClick={this.search}
+            >Search books</Link>
+        {this.state.showSearchPage && (
+          <Route exact path='/search' render={() => (
+            <div className="search-books">
+                  <div className="search-books-bar">
+                  <Link
+                    to='/'
+                    className='close-search'
+                    onClick={this.clearQuery}
+                  ></Link>
+                  <div className="search-books-input-wrapper">
+                    {/*
+                      NOTES: The search from BooksAPI is limited to a particular set of search terms.
+                      You can find these search terms here:
+                      https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
 
-                    However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                    you don't find a specific author or title. Every search is limited by search terms.
-                  */}
-                  <input type="text" value={this.state.query} onChange={(event) => this.updateQuery(event.target.value)} placeholder="Search by title or author"/>
+                      However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
+                      you don't find a specific author or title. Every search is limited by search terms.
+                    */}
+                    <input type="text" value={this.state.query} onChange={(event) => this.updateQuery(event.target.value)} placeholder="Search by title or author"/>
+                  </div>
+                </div>
+                <div className="search-books-results">
+                  <ol className="books-grid"></ol>
+                </div>
+              </div> 
+          )}/>
+        )}
+        <Route path='/' render={({ history }) => (
+            <div className="list-books">
+              <div className="list-books-title">
+                <h1>MyReads</h1>
+              </div>
+              <div className="list-books-content">
+                <div>
+                  <div className="bookshelf">
+                    <h2 className="bookshelf-title">Currently Reading</h2>
+                    <ListBooks
+                    onChangeBookStatus={this.changeBookStatus}
+                    books={showingBooks}
+                    shelf="currentlyReading"
+                    />
+                  </div>
+                  <div className="bookshelf">
+                    <h2 className="bookshelf-title">Want to Read</h2>
+                    <ListBooks
+                    onChangeBookStatus={this.changeBookStatus}
+                    books={showingBooks}
+                    shelf="wantToRead"
+                    />
+                  </div>
+                  <div className="bookshelf">
+                    <h2 className="bookshelf-title">Read</h2>
+                    <ListBooks
+                    onChangeBookStatus={this.changeBookStatus}
+                    books={showingBooks}
+                    shelf="read"
+                    />
+                  </div>
                 </div>
               </div>
-              <div className="search-books-results">
-                <ol className="books-grid"></ol>
-              </div>
-            </div> 
-        )}/>
-      )}
-      <Route path='/' render={({ history }) => (
-          <div className="list-books">
-            <div className="list-books-title">
-              <h1>MyReads</h1>
-            </div>
-            <div className="list-books-content">
-              <div>
-                <div className="bookshelf">
-                  <h2 className="bookshelf-title">Currently Reading</h2>
-                  <ListBooks
-                  onChangeBookStatus={this.changeBookStatus}
-                  books={showingBooks}
-                  shelf="currentlyReading"
-                  />
-                </div>
-                <div className="bookshelf">
-                  <h2 className="bookshelf-title">Want to Read</h2>
-                  <ListBooks
-                  onChangeBookStatus={this.changeBookStatus}
-                  books={showingBooks}
-                  shelf="wantToRead"
-                  />
-                </div>
-                <div className="bookshelf">
-                  <h2 className="bookshelf-title">Read</h2>
-                  <ListBooks
-                  onChangeBookStatus={this.changeBookStatus}
-                  books={showingBooks}
-                  shelf="read"
-                  />
-                </div>
+              <div className="open-search">
+                <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
               </div>
             </div>
-            <div className="open-search">
-              <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
-            </div>
-          </div>
-    )}/>
+      )}/>
     </div>
     )
   }
