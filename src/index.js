0feb18e364cc0622e8aa0c95f0238c551b803f0b
王晓峰 <wangxiaofeng@wangxiaofengs-iMac.local>
2018-01-14 18:01:30 +0800

    improved

--- src/index.js
+++ src/index.js
@@ -3,6 +3,5 @@ import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import './index.css'
-import './pure.min.css'
 
 ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, document.getElementById('root'))
